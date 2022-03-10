b815201c55664905d45333324be59c4d99ee58fc
jhin93 <scene1993@gmail.com>
2022-03-10 23:40:38 +0900

    test

--- client/src/dashboard/Chart.js
+++ client/src/dashboard/Chart.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from './Title';
-
+// test
 // Generate Sales Data
 function createData(time, amount) {
   return { time, amount };
