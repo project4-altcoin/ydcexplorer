f211ecc89f30d3a8765f98aed335599424434f2f
jhin93 <scene1993@gmail.com>
2022-03-17 17:29:03 +0900

    chart complete

--- client/src/dashboard/Chart.js
+++ client/src/dashboard/Chart.js
@@ -80,7 +80,7 @@ export default function Chart() {
       i++
 
       // blockdata in array
-      setallData(allData => [...allData, response.data.result])
+      // setallData(allData => [...allData, response.data.result])
       setTmArr(TmArr => [...TmArr, response.data.result.time])
       setrealTmArr(realTmArr => [...realTmArr, Unix_timestamp(response.data.result.time)])
       settxLeng(txLeng => [...txLeng, response.data.result.tx.length])
@@ -98,43 +98,75 @@ export default function Chart() {
   useEffect(() => {
     setTimeout(() => {
       txHistory();
-    }, 3000);
+    }, 5000);
   },[]);
 
   // console.log("blockData is what?", blockData)
   // console.log("blockHash is what?", blockHash)
   // console.log("blockHeight is what?", blockHeight)
-  console.log("txTime is what?", txTime)
-  console.log("txRealtime is what?", txRealtime)
+
+  // console.log("txTime is what?", txTime)
+  // console.log("txRealtime is what?", txRealtime)
+
   // console.log("txArray is  what?", txArray)
   // console.log("txNum is what?", txNum)
   
-  console.log("allData is every data of each block : ", allData)
-  console.log("TmArr is array of unixtimestamp : ", TmArr)
-  console.log("realTmArr is array of realTime : ", realTmArr)
-  console.log("txLeng is array of transaction number : ", txLeng)
+  // console.log("allData is every data of each block : ", allData)
+  // console.log("TmArr is array of unixtimestamp : ", TmArr)
+  // console.log("realTmArr is array of realTime : ", realTmArr)
+  // console.log("txLeng is array of transaction number : ", txLeng)
+
   // console.log("blockObj is object of time & txnum : ", blockObj)
 
+  let txNumSum0 = 0;
+  let txNumSum1 = 0;
+  let txNumSum2 = 0;
+  let txNumSum3 = 0;
+  let txNumSum4 = 0;
+  let txNumSum5 = 0;
+  let txNumSum6 = 0;
+  let txNumSum7 = 0;
 
+  for(let i = 1; i < 110; i ++){
+    if(TmArr[i] <= TmArr[8]){
+      txNumSum0 += txLeng[i]
+    } else if(TmArr[8] < TmArr[i] && TmArr[i] <= TmArr[18]) {
+      txNumSum1 += txLeng[i]
+    } else if(TmArr[18] < TmArr[i] && TmArr[i] <= TmArr[28]) {
+      txNumSum2 += txLeng[i]
+    } else if(TmArr[28] < TmArr[i] && TmArr[i] <= TmArr[46]) {
+      txNumSum3 += txLeng[i]
+    } else if(TmArr[46] < TmArr[i] && TmArr[i] <= TmArr[62]) {
+      txNumSum4 += txLeng[i]
+    } else if(TmArr[62] < TmArr[i] && TmArr[i] <= TmArr[78]) {
+      txNumSum5 += txLeng[i]
+    } else if(TmArr[78] < TmArr[i] && TmArr[i] <= TmArr[90]) {
+      txNumSum6 += txLeng[i]
+    } else if(TmArr[90] < TmArr[i] && TmArr[i] <= TmArr[100]) {
+      txNumSum7 += txLeng[i]
+    }
+  }
 
+  // console.log("==============txNum0===========", txNumSum0)
+  // console.log("==============txNum1===========", txNumSum1)
+  // console.log("==============txNum2===========", txNumSum2)
+  // console.log("==============txNum3===========", txNumSum3)
+  // console.log("==============txNum4===========", txNumSum4)
+  // console.log("==============txNum5===========", txNumSum5)
+  // console.log("==============txNum6===========", txNumSum6)
+  // console.log("==============txNum7===========", txNumSum7)
 
   const data = [
-    createData(realTmArr[0], 0),
-    createData(realTmArr[8], 3),
-    createData(realTmArr[18], 6),
-    createData(realTmArr[28], 8),
-    createData(realTmArr[46], 15),
-    createData(realTmArr[62], 20),
-    createData(realTmArr[78], 24),
-    createData(realTmArr[90], 24),
-    createData(realTmArr[100], undefined),
+    createData(realTmArr[0], txNumSum0),
+    createData(realTmArr[8], txNumSum1),
+    createData(realTmArr[18], txNumSum2),
+    createData(realTmArr[28], txNumSum3),
+    createData(realTmArr[46], txNumSum4),
+    createData(realTmArr[62], txNumSum5),
+    createData(realTmArr[78], txNumSum6),
+    createData(realTmArr[90], txNumSum7)
   ];
 
-
-
-  function splitTime () {
-
-  };
 
   const theme = useTheme();
 
